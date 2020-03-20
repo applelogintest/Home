c6a9ea1f81f5c38d5db15aaec9e76d7a14a82620
HakMin <kimys260@naver.com>
2020-03-20 18:03:21 +0900

    2020-03-20 JSTL

---
+++ JSPMiddleProject2/src/com/sist/dao/Sawon.java
@@ -0,0 +1,33 @@
+package com.sist.dao;
+import java.util.*;
+public class Sawon {
+	private int sabun;
+	private String name;
+	private String dept;
+	public int getSabun() {
+		return sabun;
+	}
+	public void setSabun(int sabun) {
+		this.sabun = sabun;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public Sawon(int sabun, String name, String dept) {
+		super();
+		this.sabun = sabun;
+		this.name = name;
+		this.dept = dept;
+	}
+	
+	
+}
